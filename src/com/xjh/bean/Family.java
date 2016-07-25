2875334cb0619d78fd6b05407929cb8dac69d32b
xujianhua <xujianhuaap@gmail.com>
2016-07-25 15:30:45 +0800

    init

---
+++ src/com/xjh/bean/Family.java
@@ -0,0 +1,12 @@
+package com.xjh.bean;
+
+import java.io.Serializable;
+
+public class Family implements Serializable{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/xjh/servlets/WelcomeServlet.java
@@ -0,0 +1,46 @@
+package com.xjh.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 
+ */
+@WebServlet(name="WelcomeServlet" ,urlPatterns={"/welcome"})
+public class WelcomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WelcomeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter writer=response.getWriter();
+		writer.println();
+		writer.println(request.getRequestURI());
+		response.sendRedirect("/GeekWeb/sister.html");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
